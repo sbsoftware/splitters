e9f50bae9766584d9908d4b25aa2540d4fd6910a
Stefan Bilharz <stefan@sbsoftware.de>
2025-06-03 21:02:47 +0200

    SPL-5 Add window title

--- src/views/application_layout.cr
+++ src/views/application_layout.cr
@@ -2,6 +2,10 @@ class ApplicationLayout < Crumble::Material::Layout
   append_to_head ApplicationStyle
   append_to_head ServiceWorkerRegistration
 
+  def window_title
+    "splitters.money"
+  end
+
   def headline
     "Splitters"
   end
