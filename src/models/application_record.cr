05f7deb2086005fdcb58cf6787735551f5592c6f
Stefan Bilharz <stefan@sbsoftware.de>
2025-12-09 21:51:27 +0100

    SPL-14 Adjust to updated orma version

--- src/models/application_record.cr
+++ src/models/application_record.cr
@@ -1,5 +1,7 @@
-class ApplicationRecord < Orma::Record
-  id_column id : Int64
+abstract class ApplicationRecord < Orma::Record
+  macro inherited
+    id_column id : Int64
+  end
 
   def self.db_connection_string
     "sqlite3://./data.db"
