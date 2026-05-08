f83385a7f324d0c9eb76afe8db5205df035570b6
Stefan Bilharz <stefan@sbsoftware.de>
2026-05-08 22:17:34 +0200

    SPL-39 Fix specs

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,25 +1,12 @@
 require "spec"
 require "crumble"
+require "sqlite3"
+require "orma"
 
-ENV["DATABASE_URL"] ||= "sqlite3::memory:"
 ENV["ORMA_CONTINUOUS_MIGRATION"] ||= "true"
 
+TEST_DB_CONNECTION_STRING = "sqlite3:%3Amemory%3A?max_pool_size=1&prepared_statements_cache=false"
+
+Orma.db_connection_string = TEST_DB_CONNECTION_STRING
+
 require "../src/environment"
-
-module SpecDB
-  @@db : DB::Database?
-
-  def self.db
-    @@db ||= DB.open(ENV["DATABASE_URL"])
-  end
-end
-
-abstract class Orma::Record
-  def self.db
-    if conn = Fiber.current._orma_current_connection
-      return conn
-    end
-
-    SpecDB.db
-  end
-end
